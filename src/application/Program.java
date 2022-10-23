d869587143aab775cdb39e87c2105db6d52e5563
fabricio-joao <fabriciojoao41@outlook.com>
2022-10-23 17:33:03 -0300

    Segundo Commit

---
+++ src/application/Program.java
@@ -0,0 +1,14 @@
+package application;
+
+import model.entidies.Department;
+
+public class Program {
+
+	public static void main(String[] args) {
+		
+		Department obj = new Department(1, "Books");
+		System.out.println(obj);
+
+	}
+
+}
--- src/db/DB.java
+++ src/db/DB.java
@@ -1,5 +1,72 @@
 package db;
 
+import java.io.FileInputStream;
+import java.io.IOException;
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.sql.Statement;
+import java.util.Properties;
+
 public class DB {
 
+	private static Connection conexao = null;
+
+	public static Connection pegarConexao() {
+		if (conexao == null) {
+			try {
+				Properties props = carregarDados();
+				String url = props.getProperty("dburl");
+				conexao = DriverManager.getConnection(url, props);
+			} catch (SQLException e) {
+				throw new DbException(e.getMessage());
+			}
+		}
+		return conexao;
+	}
+
+	public static void fecharConexao() {
+		if (conexao != null) {
+			try {
+				conexao.close();
+			} catch (SQLException e) {
+				throw new DbException(e.getMessage());
+			}
+		}
+	}
+
+	private static Properties carregarDados() {
+		try (FileInputStream fs = new FileInputStream("db.properties")) {
+
+			Properties props = new Properties();
+			props.load(fs);
+			return props;
+
+		} catch (IOException e) {
+			throw new DbException(e.getMessage());
+		}
+	}
+
+	// metodo para fechar statements
+	public static void fecharStatements(Statement st) {
+		if (st != null) {
+			try {
+				st.close();
+			} catch (SQLException e) {
+				throw new DbException(e.getMessage());
+			}
+		}
+	}
+
+	// metodo para fechar ResultSet
+	public static void fecharResultSet(ResultSet rs) {
+		if (rs != null) {
+			try {
+				rs.close();
+			} catch (SQLException e) {
+				throw new DbException(e.getMessage());
+			}
+		}
+	}
 }
