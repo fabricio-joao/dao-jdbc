7c094650abb5e5e5e7b16b1228762134e43dd972
fabricio-joao <fabriciojoao41@outlook.com>
2022-10-23 18:46:00 -0300

    Quinto Commit

--- src/model/dao/SellerDao.java
+++ src/model/dao/SellerDao.java
@@ -2,13 +2,13 @@ package model.dao;
 
 import java.util.List;
 
-import model.entidies.Seller;
+import model.entidies.Department;
 
 public interface SellerDao {
 
-	void insert(Seller obj);
-	void update(Seller obj);
-	void deleteById(Seller obj);
-	Seller findById(Integer id);
-    List<Seller> findAll();
+	void insert(Department obj);
+	void update(Department obj);
+	void deleteById(Department obj);
+	Department findById(Integer id);
+	List<Department> findAll();
 }
